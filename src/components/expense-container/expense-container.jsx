edea410fb3d311689df73f3f22fea0f2d0ab6520
imsahiljain <emailofsahiljain@gmail.com>
2022-01-28 19:48:36 +0530

    add expense not found condition

--- src/components/expense-container/expense-container.jsx
+++ src/components/expense-container/expense-container.jsx
@@ -1,19 +1,28 @@
+import { Center, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import ExpenseCard from "../expense-card/expense-card";
 
 const ExpenseContainer = (props) => {
   return (
     <div>
-      {props.expenses.map((expense) => {
-        return (
-          <ExpenseCard
-            key={expense.id}
-            name={expense.name}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
+      {props.expenses.length === 0 && (
+        <Center>
+          <Text fontSize="md" fontFamily="Inter" mt="150px" color="gray.400">
+            No expense found this year. Add from above, if any.
+          </Text>
+        </Center>
+      )}
+      {props.expenses.length > 0 &&
+        props.expenses.map((expense) => {
+          return (
+            <ExpenseCard
+              key={expense.id}
+              name={expense.name}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
     </div>
   );
 };
