1a943c017bda0444f06c9efc276229c54a28df06
imsahiljain <emailofsahiljain@gmail.com>
2022-01-28 11:21:04 +0530

    add navbar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Flex, Spacer, Box, Heading, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/button";
+import "../navbar/navbar.css";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Flex boxShadow="md">
+      <Box p="2" m="4">
+        <Heading className="navHeading" fontFamily="Inter" fontSize="2xl">
+          Expense Tracker
+        </Heading>
+      </Box>
+      <Spacer />
+      <Box m="5">
+        <IconButton mr="4" aria-label="Toggle Mode" onClick={toggleColorMode}>
+          {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+        </IconButton>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
