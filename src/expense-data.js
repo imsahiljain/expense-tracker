fc47849bc085fda2b2c939b21176eee89304d953
imsahiljain <emailofsahiljain@gmail.com>
2022-01-28 15:41:35 +0530

    add dummy EXPENSE_DATA

---
+++ src/expense-data.js
@@ -0,0 +1,22 @@
+const EXPENSES_DATA = [
+  {
+    id: 1,
+    name: "Grocery",
+    amount: "300",
+    date: new Date(2022, 1, 28).toLocaleString("en-GB").slice(0, 10),
+  },
+  {
+    id: 2,
+    name: "Restaraunt Order",
+    amount: "1300",
+    date: new Date(2022, 1, 28).toLocaleString("en-GB").slice(0, 10),
+  },
+  {
+    id: 3,
+    name: "Phone Recharge",
+    amount: "200",
+    date: new Date(2022, 1, 28).toLocaleString("en-GB").slice(0, 10),
+  },
+];
+
+export default EXPENSES_DATA;
