ff187d5e18805b9863eb380a59ece458725ffbb9
imsahiljain <emailofsahiljain@gmail.com>
2022-01-28 15:43:11 +0530

    set new data + add data to state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/navbar/navbar";
 import Form from "./components/form/form";
 import ExpenseContainer from "./components/expense-container/expense-container";
+import EXPENSES_DATA from "./expense-data.js";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState();
+
+  const getNewData = (newExpenseData) => {
+    const newData = {
+      id: Math.random(),
+      ...newExpenseData,
+    };
+    setData((prevState) => {
+      return [newData, ...prevState];
+    });
+  };
+
   return (
     <>
       <Navbar />
-      <Form />
-      <ExpenseContainer />
+      <Form getNewData={getNewData} />
+      <ExpenseContainer expenses={data} />
     </>
   );
-}
+};
 
 export default App;
